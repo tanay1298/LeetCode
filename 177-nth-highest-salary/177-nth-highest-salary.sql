922287e68022634c36a4c563f956ee7502418259
Tanay Burreja <32588346+tanay1298@users.noreply.github.com>
2022-04-19 23:59:51 +0530

    Time: 394 ms (36.37%), Space: 0B (100.00%) - LeetHub

---
+++ 177-nth-highest-salary/177-nth-highest-salary.sql
@@ -0,0 +1,11 @@
+# LIMIT offset, count;  
+
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+SET N = N-1;
+  RETURN (
+      select distinct(salary) from Employee
+      order by salary desc 
+      limit N,1
+  );
+END
