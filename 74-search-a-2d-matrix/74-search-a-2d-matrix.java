6fc4f9156e214f15d69debb5cef5a18546431247
Tanay Burreja <32588346+tanay1298@users.noreply.github.com>
2022-09-14 08:58:51 +0530

    Time: 25 ms (5.04%), Space: 43.7 MB (5.37%) - LeetHub

---
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.java
@@ -0,0 +1,62 @@
+class Solution {
+    
+    public boolean search(int[][]matrix, int i, int j, int target, int m, int n)
+    {
+        System.out.println("in helper");
+        System.out.print("i is " + i);
+        System.out.print(" j is " + j);
+        boolean res1 = false;
+        boolean res2 = false;
+        
+        if(i>=0 && j<n)
+        {
+            int x = matrix[i][j];
+            
+            if(target == x)
+                return true;
+            
+            if(target < x && j-1 >= 0)
+            {
+                // if(target <= matrix[i][j-1])
+                // {
+                    System.out.print("less");
+                    res1 = search(matrix, i, j-1, target, m, n);
+                    if(res1 == true)
+                        return true;
+                // }
+                
+            }
+            
+        
+            if(target > x && i+1 < m)
+            {
+                // if(target >= matrix[i+1][j])
+                // {
+                    System.out.print("more");
+                    res2 = search(matrix, i+1, j, target, m , n);
+                    if(res2 == true)
+                        return true;
+                // }
+                
+            }
+        }
+        
+        return false;
+    }
+    
+    public boolean searchMatrix(int[][] matrix, int target) 
+    {
+       int m = matrix.length;
+       int n = matrix[0].length; 
+       int i = 0;
+       int j = n-1; 
+       int x = matrix[i][j];
+       
+        
+        if(x==target)
+            return true;
+        
+        boolean res = search(matrix, i, j, target, m , n);
+        return res;
+    }
+}
