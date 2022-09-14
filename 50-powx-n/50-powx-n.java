08bfa49968a2846549c4c2b173aab991f53a4a16
Tanay Burreja <32588346+tanay1298@users.noreply.github.com>
2022-09-14 09:47:45 +0530

    Time: 1 ms (89.80%), Space: 42.9 MB (51.04%) - LeetHub

---
+++ 50-powx-n/50-powx-n.java
@@ -0,0 +1,40 @@
+class Solution 
+{
+    
+    public double myPow(double x, int n) 
+    {
+        if(n==0)
+            return 1;
+        
+        if(n==1)
+            return x;
+        
+        if(x==1)
+            return 1;
+        
+        if(x == -1)
+        {
+            if(n%2==0)
+                return 1;
+            else
+                return -1;
+        }
+        
+        if(n == -2147483648)
+            return 0;
+        
+        if(n < 0)
+        {
+           n = -n;
+           x = 1/x; 
+        }
+ 
+        double xnb2 = myPow(x, n/2);
+        double xn = xnb2 * xnb2;    
+        
+        if(n%2 == 1)
+            xn = x * xn;
+        
+        return xn;
+    }
+}
